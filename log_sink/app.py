721f86885e7d93f400d1453c9421cd988fb01271
Tarcisio Oliveira <t@rcis.io>
2024-08-07 02:13:42 +0000

    update

--- log_sink/app.py
+++ log_sink/app.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 def log():
 
     data = {
-#        'headers' : to_dict(request.headers),
+        'headers' : to_dict(request.headers),
         'args'    : to_dict(request.args),
         'form'    : to_dict(request.form),
         'files'   : to_dict(request.files),
         'json'    : request.json if request.is_json else {}
     }
 
-    #data = dumps(data, indent = 4)
+    data = dumps(data, indent = 4)
     print(data)
 
     return data
 
 
-def to_dict(data): return data#.to_dict(flat = False) if 'to_dict' in dir(data) else dict(data)
+def to_dict(data): return data.to_dict(flat = False) if 'to_dict' in dir(data) else dict(data)
